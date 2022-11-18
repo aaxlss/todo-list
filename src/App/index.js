c0962277daeba2dd1e19ad3ff1aed2a1c7227f88
aaxlss <axl9305@gmail.com>
2022-11-18 13:35:18 +0000

    Revert "[Update] Upadting the structure of components to call everything together"
    This reverts commit c7496ce6b1312989dcbb4bfd291872f1a0acf16e.

--- src/App/index.js
+++ src/App/index.js
@@ -1,62 +1,18 @@
 import React, { useEffect, useState } from "react";
-// import {TodoProvider} from '../TodoContext'
-// import { TodoItem } from "../TodoItem";
-// import { TodoList } from "../TodoList";
+import {TodoProvider} from '../TodoContext'
 import "./App.css";
 import { AppUI } from "./AppUI";
 
 
 
 function App() {
+  
 
-  const [state, setState] = useState('Estado compartido');
   return (
-    <React.Fragment>
-      <TodoHeader>
-        <TodoCounter/>
-        <TodoSearch/>
-      </TodoHeader>
-      <TodoList>
-        <TodoItem state={state}/>
-      </TodoList>
-      </React.Fragment>
+    <TodoProvider>
+      <AppUI/>
+    </TodoProvider>
   );
 }
-
-function TodoHeader ({children}){
-  return (
-    <header>
-      {children}
-    </header>
-  )
-}
-
-function TodoCounter(){
-  return(
-    <p>TodoCounter</p>
-  )
-}
-
-function TodoSearch(){
-  return (
-    <p>TodoSearch</p>
-  )
-}
-
-function TodoList({children}){
-  return (
-    <React.Fragment>
-      {children}
-    </React.Fragment>
-  )
-}
-
-function TodoItem({state}){
-  return (
-    <p>TodoItem: {state}</p>
-  )
-
-}
-
 
 export default App;
