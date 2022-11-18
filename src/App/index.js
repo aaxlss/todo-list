c7496ce6b1312989dcbb4bfd291872f1a0acf16e
aaxlss <axl9305@gmail.com>
2022-11-18 13:27:12 +0000

    [Update] Upadting the structure of components to call everything together

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,62 @@
 import React, { useEffect, useState } from "react";
-import {TodoProvider} from '../TodoContext'
+// import {TodoProvider} from '../TodoContext'
+// import { TodoItem } from "../TodoItem";
+// import { TodoList } from "../TodoList";
 import "./App.css";
 import { AppUI } from "./AppUI";
 
 
 
 function App() {
-  
 
+  const [state, setState] = useState('Estado compartido');
   return (
-    <TodoProvider>
-      <AppUI/>
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter/>
+        <TodoSearch/>
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state}/>
+      </TodoList>
+      </React.Fragment>
   );
 }
+
+function TodoHeader ({children}){
+  return (
+    <header>
+      {children}
+    </header>
+  )
+}
+
+function TodoCounter(){
+  return(
+    <p>TodoCounter</p>
+  )
+}
+
+function TodoSearch(){
+  return (
+    <p>TodoSearch</p>
+  )
+}
+
+function TodoList({children}){
+  return (
+    <React.Fragment>
+      {children}
+    </React.Fragment>
+  )
+}
+
+function TodoItem({state}){
+  return (
+    <p>TodoItem: {state}</p>
+  )
+
+}
+
 
 export default App;
