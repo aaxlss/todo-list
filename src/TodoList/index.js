dc5d0a4b9a4b79b1356b826c5b2be8df4e57867d
aaxlss <axl9305@gmail.com>
2022-11-21 13:00:37 +0000

    [Fix] Fixing error calling method

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -5,7 +5,7 @@ function TodoList(props){
         <section className="TodoList-container">
             {props.error && props.onError()}
             {props.loading && props.onLoading()}
-            {(!props.loading && !props.searchedTodos.length)&& props.onEmpty()}
+            {(!props.loading && !props.searchedTodos.length)&& props.onEmptyTodos()}
             {props.searchedTodos.map(props.render)}
             {}
             <ul>
